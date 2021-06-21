94c802a77cde32ac9cfeca0638b644dae5ab6afa
SeoBoSeok <ggy7bbo@gmail.com>
2021-06-21 19:46:49 +0900

    hot fix

--- theme/basic/portfolio.php
+++ theme/basic/portfolio.php
@@ -8,7 +8,7 @@ if (G5_IS_MOBILE) {
 }
 
 include_once(G5_THEME_PATH.'/head-new.php');
-$sql = " SELECT a.wr_subject, a.wr_content, a.wr_link1, a.wr_1, b.bf_file FROM makeproduction.g5_write_makeproduction a join makeproduction.g5_board_file b on b.wr_id = a.wr_id WHERE b.bo_table = 'makeproduction'";
+$sql = " SELECT wr_subject, wr_content, wr_link1, wr_1 FROM makeproduction.g5_write_makeproduction ";
 $result = sql_query($sql);
 ?>
 <div class="wrap">
