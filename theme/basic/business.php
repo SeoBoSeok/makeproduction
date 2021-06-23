9df1def4cb25481b3b658e50004debae8099ee86
SeoBoSeok <ggy7bbo@gmail.com>
2021-06-24 05:49:40 +0900

    business hot fix

--- theme/basic/business.php
+++ theme/basic/business.php
@@ -20,7 +20,6 @@ include_once(G5_THEME_PATH.'/head-new.php');
 					<main id="main" class="site-main" role="main">
 						<article id="post-2" class="post-2 page type-page status-publish hentry">
 							<div class="entry-content">
-
 								<div class="front-section" id="section_2_1">
                   <div class="inner">
                     <div class="title">BUSINESS</div>
@@ -33,7 +32,74 @@ include_once(G5_THEME_PATH.'/head-new.php');
                       </div>
                     </div>
                     <div class="table">
+<!--                       
+Array
+(
+    [wr_id] => 1
+    [wr_num] => -1
+    [wr_reply] => 
+    [wr_parent] => 1
+    [wr_is_comment] => 0
+    [wr_comment] => 0
+    [wr_comment_reply] => 
+    [ca_name] => 2021
+    [wr_option] => 
+    [wr_subject] => 메이크 프로덕션 공주문화여행 메이킹 필름
+    [wr_content] => 메이크 프로덕션 공주문화여행 메이킹 필름 메이크 프로덕션은 다양한 분야의 크리에이티브를 제작하고 있습니다. 2021. 06. 06 ~ 2021. 06. 06
+    [wr_seo_title] => 메이크-프로덕션-공주문화여행-메이킹-필름
+    [wr_link1] => 
+    [wr_link2] => 
+    [wr_link1_hit] => 0
+    [wr_link2_hit] => 0
+    [wr_hit] => 1
+    [wr_good] => 0
+    [wr_nogood] => 0
+    [mb_id] => admin
+    [wr_password] => 
+    [wr_name] => 최고관리자
+    [wr_email] => admin@domain.com
+    [wr_homepage] => 
+    [wr_datetime] => 2021-06-24 05:22:47
+    [wr_file] => 1
+    [wr_last] => 2021-06-24 05:22:47
+    [wr_ip] => ::1
+    [wr_facebook_user] => 
+    [wr_twitter_user] => 
+    [wr_1] => 메이크 프로덕션은 다양한 분야의 크리에이티브를 제작하고 있습니다.
+    [wr_2] => 2021. 06. 06 ~ 2021. 06. 06
+    [wr_3] => 
+    [wr_4] => 
+    [wr_5] => 
+    [wr_6] => 
+    [wr_7] => 
+    [wr_8] => 
+    [wr_9] => 
+    [wr_10] => 
+)
+-->
+                      <?php
+                        // SELECT a.wr_subject, a.wr_content, a.wr_link1, a.wr_1, b.bf_file FROM makeproduction.g5_write_makeproduction a join makeproduction.g5_board_file b on b.wr_id = a.wr_id WHERE b.bo_table = 'makeproduction'
+                        $sql = " select a.*, b.bf_file from makeproduction.g5_write_business a join makeproduction.g5_board_file b on b.wr_id = a.wr_id WHERE b.bo_table = 'business' ";
+                        $result = sql_query($sql);
+                        for($i=0; $row=sql_fetch_array($result); $i++) {
+                      ?>
                       <div class="items">
+                        <div class="item__title">
+                          <div class="left"><?= $row['ca_name'] ?></div>
+                          <div class="right">
+                            <div class="sub__title"><?= $row['wr_subject'] ?></div>
+                            <div class="sub__dsecription"><?= $row['wr_1'] ?></div>
+                            <div class="sub__date"><?= $row['wr_2'] ?></div>
+                          </div>
+                        </div>
+                        <div class="item__description" style="background-image: url('/data/file/business/<?= $row['bf_file'] ?>')">
+                            
+                        </div>
+                      </div>
+                      <?php
+                      }
+                      ?>
+                      <!-- <div class="items">
                         <div class="item__title">
                           <div class="left">2019</div>
                           <div class="right">
@@ -84,20 +150,7 @@ include_once(G5_THEME_PATH.'/head-new.php');
                         <div class="item__description">
 
                         </div>
-                      </div>
-                      <div class="items">
-                        <div class="item__title">
-                          <div class="left">2019</div>
-                          <div class="right">
-                            <div class="sub__title">메이크 프로덕션 공주문화여행 메이킹 필름</div>
-                            <div class="sub__dsecription">메이크 프로덕션은 다양한 분야의 크리에이티브를 제작하고 있습니다.</div>
-                            <div class="sub__date">2021. 06. 06 ~ 2021. 06. 06</div>
-                          </div>
-                        </div>
-                        <div class="item__description">
-
-                        </div>
-                      </div>
+                      </div> -->
                     </div>
                   </div>
 								</div>
