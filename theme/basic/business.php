9f8c8d05e8546fa9984197cc6443de93416f41d8
SeoBoSeok <ggy7bbo@gmail.com>
2021-06-30 03:39:32 +0900

    hot fix

--- theme/basic/business.php
+++ theme/basic/business.php
@@ -54,58 +54,9 @@ include_once(G5_THEME_PATH.'/head-new.php');
                               }
                               print_r($str);
                             ?>
-                            <!-- <div class="cat_item">ALL</div>
-                            <div class="cat_item">2021</div>
-                            <div class="cat_item">2020</div>
-                            <div class="cat_item">2019</div> -->
                           </div>
                         </div>
                         <div class="table">
-    <!--                       
-    Array
-    (
-        [wr_id] => 1
-        [wr_num] => -1
-        [wr_reply] => 
-        [wr_parent] => 1
-        [wr_is_comment] => 0
-        [wr_comment] => 0
-        [wr_comment_reply] => 
-        [ca_name] => 2021
-        [wr_option] => 
-        [wr_subject] => 메이크 프로덕션 공주문화여행 메이킹 필름
-        [wr_content] => 메이크 프로덕션 공주문화여행 메이킹 필름 메이크 프로덕션은 다양한 분야의 크리에이티브를 제작하고 있습니다. 2021. 06. 06 ~ 2021. 06. 06
-        [wr_seo_title] => 메이크-프로덕션-공주문화여행-메이킹-필름
-        [wr_link1] => 
-        [wr_link2] => 
-        [wr_link1_hit] => 0
-        [wr_link2_hit] => 0
-        [wr_hit] => 1
-        [wr_good] => 0
-        [wr_nogood] => 0
-        [mb_id] => admin
-        [wr_password] => 
-        [wr_name] => 최고관리자
-        [wr_email] => admin@domain.com
-        [wr_homepage] => 
-        [wr_datetime] => 2021-06-24 05:22:47
-        [wr_file] => 1
-        [wr_last] => 2021-06-24 05:22:47
-        [wr_ip] => ::1
-        [wr_facebook_user] => 
-        [wr_twitter_user] => 
-        [wr_1] => 메이크 프로덕션은 다양한 분야의 크리에이티브를 제작하고 있습니다.
-        [wr_2] => 2021. 06. 06 ~ 2021. 06. 06
-        [wr_3] => 
-        [wr_4] => 
-        [wr_5] => 
-        [wr_6] => 
-        [wr_7] => 
-        [wr_8] => 
-        [wr_9] => 
-        [wr_10] => 
-    )
-    -->
                           <?php
                             // SELECT a.wr_subject, a.wr_content, a.wr_link1, a.wr_1, b.bf_file FROM makeproduction.g5_write_makeproduction a join makeproduction.g5_board_file b on b.wr_id = a.wr_id WHERE b.bo_table = 'makeproduction'
                             if (!empty($_GET['caname'])) {
@@ -119,7 +70,7 @@ include_once(G5_THEME_PATH.'/head-new.php');
                             for ($i=0; $row=sql_fetch_array($result); $i++) {
                           ?>
                           <div class="items">
-                            <div class="item__title">
+                            <a class="item__title" href="/?p=details&bo_table=business&wr_id=<?=$row['wr_id']?>&ca_name=<?=$row['ca_name']?>">
                               <div class="left"><?= $row['ca_name'] ?>
                                 <div class="month">
                                   <?= substr($row['wr_2'], 0, 2) ?>
@@ -129,7 +80,7 @@ include_once(G5_THEME_PATH.'/head-new.php');
                                 <div class="sub__title"><?= $row['wr_subject'] ?></div>
                                 <div class="sub__dsecription"><?= $row['wr_1'] ?></div>
                               </div>
-                            </div>
+                            </a>
                             <?php
                               if (!empty($row['wr_link1'])) {
                                 preg_match('/^.*(?:(?:youtu\\.be\\/|v\\/|vi\\/|u\\/\\w\\/|embed\\/)|(?:(?:watch)?\\?v(?:i)?=|\\&v(?:i)?=))([^#\\&\\?]*).*/', $row['wr_link1'], $matches);
@@ -156,58 +107,6 @@ include_once(G5_THEME_PATH.'/head-new.php');
                             echo '<div class="container"><div class="row"><div class="col text-center">컨테츠 준비중입니다.</div></div></div>';
                           }
                           ?>
-                          <!-- <div class="items">
-                            <div class="item__title">
-                              <div class="left">2019</div>
-                              <div class="right">
-                                <div class="sub__title">메이크 프로덕션 공주문화여행 메이킹 필름</div>
-                                <div class="sub__dsecription">메이크 프로덕션은 다양한 분야의 크리에이티브를 제작하고 있습니다.</div>
-                                <div class="sub__date">2021. 06. 06 ~ 2021. 06. 06</div>
-                              </div>
-                            </div>
-                            <div class="item__description">
-    
-                            </div>
-                          </div>
-                          <div class="items">
-                            <div class="item__title">
-                              <div class="left">2019</div>
-                              <div class="right">
-                                <div class="sub__title">메이크 프로덕션 공주문화여행 메이킹 필름</div>
-                                <div class="sub__dsecription">메이크 프로덕션은 다양한 분야의 크리에이티브를 제작하고 있습니다.</div>
-                                <div class="sub__date">2021. 06. 06 ~ 2021. 06. 06</div>
-                              </div>
-                            </div>
-                            <div class="item__description">
-    
-                            </div>
-                          </div>
-                          <div class="items">
-                            <div class="item__title">
-                              <div class="left">2019</div>
-                              <div class="right">
-                                <div class="sub__title">메이크 프로덕션 공주문화여행 메이킹 필름</div>
-                                <div class="sub__dsecription">메이크 프로덕션은 다양한 분야의 크리에이티브를 제작하고 있습니다.</div>
-                                <div class="sub__date">2021. 06. 06 ~ 2021. 06. 06</div>
-                              </div>
-                            </div>
-                            <div class="item__description">
-    
-                            </div>
-                          </div>
-                          <div class="items">
-                            <div class="item__title">
-                              <div class="left">2019</div>
-                              <div class="right">
-                                <div class="sub__title">메이크 프로덕션 공주문화여행 메이킹 필름</div>
-                                <div class="sub__dsecription">메이크 프로덕션은 다양한 분야의 크리에이티브를 제작하고 있습니다.</div>
-                                <div class="sub__date">2021. 06. 06 ~ 2021. 06. 06</div>
-                              </div>
-                            </div>
-                            <div class="item__description">
-    
-                            </div>
-                          </div> -->
                         </div>
 
                       </div>
---
+++ theme/basic/details.php
@@ -0,0 +1,98 @@
+<?php
+define('_INDEX_', true);
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+if (G5_IS_MOBILE) {
+    include_once(G5_THEME_MOBILE_PATH.'/index.php');
+    return;
+}
+
+if (empty($_GET['wr_id'])) exit;
+
+include_once(G5_THEME_PATH.'/head-new.php');
+
+if (!empty($_GET['caname'])) {
+  $sql = " SELECT a.*, b.bf_file FROM makeproduction.g5_write_business a JOIN makeproduction.g5_board_file b ON b.wr_id = a.wr_id WHERE ca_name = {$_GET['caname']} AND a.wr_id = {$_GET['wr_id']} LIMIT 1 ";
+} else {
+  $sql = " SELECT a.*, b.bf_file FROM makeproduction.g5_write_business a JOIN makeproduction.g5_board_file b ON b.wr_id = a.wr_id WHERE a.wr_id = {$_GET['wr_id']} LIMIT 1 ";
+}
+// print_r($sql);
+$result = sql_query($sql);
+$total_rows = mysqli_num_rows($result);
+?>
+<div class="wrap">
+	<?php
+    include_once(G5_THEME_PATH.'/header_part.php');
+  ?>
+		<div class="site-content-contain">
+
+			<div id="content" class="site-content">
+				<div id="primary" class="content-area">
+					<main id="main" class="site-main" role="main">
+						<article id="post-2" class="post-2 page type-page status-publish hentry">
+							<div class="entry-content">
+								<div class="front-section" id="section_2_1">
+                  <?php
+                    for ($i=0; $row=sql_fetch_array($result); $i++) {
+                  ?>
+                  <div class="container" id="business__container">
+                    <div class="row">
+                      <div class="col col-lg-offset-2 col-lg-8 col-md-offset-2 col-md-8 col-sm-offset-3 col-sm-6">
+                        <div class="title"><?= $row['wr_subject'] ?></div>
+                        <div id="sb-wrap">
+                          <div id="sb-view">
+
+                            <!-- 타이틀 영역 -->
+                            <div class="titWrap">
+                              <h5 class="sbj">세계맥주할인점 내부 </h5>
+                              <ul class="info">
+                                <li><em>작성일</em>2020/03/25 17:55</li>
+                                <li><em>조회</em> 1,745</li>
+                              </ul>
+                            </div>
+
+                              
+                            <!-- 내용 영역 -->
+                            <div class="memoWrap nostyle">
+                              <?=$row['wr_content']?>
+                            </div>
+
+                            <!-- s:foot button -->
+                            <div id="sb-footer" class="no-abs">
+                              <div class="left">
+                                <a href="/kor/board/interior2?ca=%ED%94%BD%EB%AF%B8%EA%B0%95%EC%A0%90&amp;sel_search=&amp;txt_search=&amp;page=1" class="sb-btn type3">목록</a>
+                                <a href="/_subpage/kor/board/interior2.php?viewMode=view&amp;idx=7&amp;ca=%ED%94%BD%EB%AF%B8%EA%B0%95%EC%A0%90&amp;sel_search=&amp;txt_search=&amp;page=1" class="sb-btn type4">이전</a>							</div>
+                              <div class="right">
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+								</div>
+								<?php
+                  }
+                ?>
+							</div>
+						</article>
+					</main>
+				</div>
+
+			</div><!-- #content -->
+		</div><!-- .site-content-contain -->
+	</div><!-- #page -->
+
+	<?php
+    include_once(G5_THEME_PATH.'/footer_part.php');
+  ?>
+<script src="/assets/js/plugins.js"></script>
+<script src="/assets/js/dsn-grid.js"></script>
+<script src="/assets/js/custom.js"></script>
+<script>
+setTimeout(function() {
+  document.querySelector('.pre-loader').style.display = "none";
+}, 500);
+</script>
+<?php
+include_once(G5_THEME_PATH.'/tail.php');
+?>
