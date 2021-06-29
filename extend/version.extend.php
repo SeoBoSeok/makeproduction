f1d0347f506285dfd5dee2237b84fd4cd5407715
SeoBoSeok <ggy7bbo@gmail.com>
2021-06-30 04:06:31 +0900

    version up

--- extend/version.extend.php
+++ extend/version.extend.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
-define('G5_JS_VER',  '210627');
-define('G5_CSS_VER', '210627');
+define('G5_JS_VER',  '210630');
+define('G5_CSS_VER', '210630');
 ?>
